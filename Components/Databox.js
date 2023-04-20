02c8b9c02181bf9b278793c764c0e2160cf19fdc
jasim-n <muhammadsr972@gmail.com>
2023-04-20 21:47:53 +0500

    task 1_2

--- Components/Databox.js
+++ Components/Databox.js
@@ -1,10 +1,14 @@
 import { Col, Image, Row } from 'antd';
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import moment from 'moment';
 const Databox = ({data}) => {
     const router = useRouter();
     const [Loadedimg,setLoadedimg] =useState(true)
     console.log(data,'inside of databox',data.id)
+    let date=data.created_at
+    const dateMoment = moment(date).format('MMMM Do ,YYYY');;
+console.log(dateMoment,'gvtbuhj');
     let a=data.id
     return (
         <div>
@@ -15,7 +19,7 @@ const Databox = ({data}) => {
                 }
                 </Col>
                 <Col span={24}><p className='paragraphst' style={{ fontFamily: "Roboto",fontSize:'14px',fontWeight:'400',lineHeight:'22px',paddingTop:'.7rem' }}>{data?.comment}</p></Col>
-                <Col span={24}><p style={{ fontFamily: "Roboto",fontSize:'12px',fontWeight:'400',lineHeight:'20px',color:'#8C8C8C',marginTop:'7px' }}>Nov 10, 2022</p></Col>
+                <Col span={24}><p style={{ fontFamily: "Roboto",fontSize:'12px',fontWeight:'400',lineHeight:'20px',color:'#8C8C8C',marginTop:'7px' }}>{dateMoment}</p></Col>
             
             </Row>
             
